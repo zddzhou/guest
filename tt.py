d8002fd6d274e51ca445896fc53e7360ec117c62
zhoudandan <605699041@qq.com>
2018-04-13 18:01:26 +0800

    h

--- tt.py
+++ tt.py
@@ -15,7 +15,6 @@ array =[3,2,1]
 for i in range(1, len(array)):
     if array[i - 1] > array[i]:
         temp = array[i]     # 当前需要排序的元素
- #       gfhjkhjkl
         # woshixianjiade dahuil
         index = i           # 用来记录排序元素需要插入的位置dsdsddsds
         while index > 0 and array[index - 1] > temp:
